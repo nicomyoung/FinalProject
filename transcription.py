e6f5a172ef824fad98968c12ef223d6f664d011f
nicomyoung <52359294+nicomyoung@users.noreply.github.com>
2023-12-05 12:40:05 -0500

    Update transcription.py
    Change to MySQL for storage

--- transcription.py
+++ transcription.py
@@ -1,96 +1,135 @@
 import os
 import json
 from google.cloud import storage, speech_v1p1beta1 as speech
-from google.cloud.exceptions import Conflict
+import mysql.connector
+from mysql.connector import Error
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
-# Set Google Cloud credentials and initialize clients
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Final Project/key.json"
-storage_client = storage.Client(project="sigma-outlook-400223")  # specifying the project
+# Define constants
+BUCKET_NAME = "fp-mp3-bucket"
+LOCAL_DIRECTORY = "C:/Final Project/localAudios/wavFiles"
+
+
+# Initialize Google Cloud clients
+storage_client = storage.Client(project=os.getenv('GOOGLE_CLOUD_PROJECT'))
 speech_client = speech.SpeechClient()
 
-# Configurations
-bucket_name = "fp-mp3-bucket"  # specifying the bucket name
-local_directory = "C:/Final Project/localAudios/wavFiles"
-json_output_directory = "C:/Final Project/jsonTranscriptions"
-if not os.path.exists(json_output_directory):
-    os.makedirs(json_output_directory)
 
-# 2. Upload WAV files to GCS
-def upload_files():
-    bucket = storage_client.bucket(bucket_name)
-    for filename in os.listdir(local_directory):
-        if filename.endswith(".wav"):
-            blob = bucket.blob(filename)
-            blob.upload_from_filename(os.path.join(local_directory, filename))
-            print(f"File {filename} uploaded to {bucket_name}.")
 
-def transcribe_audio(gcs_uri):
-    audio = speech.RecognitionAudio(uri=gcs_uri)
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=44100,
-        language_code="en-US",
-        audio_channel_count = 2,
-        enable_automatic_punctuation=True,
-        enable_word_time_offsets=True,
-        enable_speaker_diarization=True,
-        diarization_speaker_count=2  # might need to adjust this
-    )
 
-    operation = speech_client.long_running_recognize(config=config, audio=audio)
-    response = operation.result()
+# Database Utility Functions
+def db_connect():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            database=os.getenv('MYSQL_DATABASE', 'chatr'),
+            user=os.getenv('MYSQL_USER'),
+            password=os.getenv('MYSQL_PASSWORD')
+        )
+        return connection
+    except Error as e:
+        print(f"Error connecting to MySQL database: {e}")
+        raise e
 
-    transcription_data = []
-    complete_transcription = ""
+
+def insert_transcription(file_name, transcription):
+    try:
+        connection = db_connect()
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                INSERT INTO transcriptions (file_name, transcription)
+                VALUES (%s, %s)
+            """, (file_name, transcription))
+            connection.commit()
+    except Error as e:
+        print(f"Error while inserting transcription: {e}")
+    finally:
+        if connection:
+            connection.close()
+
+
+def check_if_transcribed(file_name):
+    try:
+        connection = db_connect()
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                SELECT EXISTS(
+                    SELECT 1 FROM transcriptions WHERE file_name = %s
+                )
+            """, (file_name,))
+            result = cursor.fetchone()
+            return result[0]
+    except Error as e:
+        print(f"Error checking if transcription exists: {e}")
+        return False
+    finally:
+        if connection:
+            connection.close()
+
+# Transcription and File Processing Classes
+class AudioTranscription:
+    def __init__(self):
+        self.storage_client = storage.Client(project="sigma-outlook-400223") 
+        self.speech_client = speech.SpeechClient()
+        self.bucket_name = BUCKET_NAME
+        
+
+    def upload_files(self, local_directory):
+        bucket = self.storage_client.bucket(self.bucket_name)
+        for filename in os.listdir(local_directory):
+            if filename.endswith(".wav"):
+                blob = bucket.blob(filename)
+                blob.upload_from_filename(os.path.join(local_directory, filename))
+                print(f"File {filename} uploaded to {self.bucket_name}.")
+
+    def transcribe_audio(self, gcs_uri):
+        audio = speech.RecognitionAudio(uri=gcs_uri)
+        config = speech.RecognitionConfig(
+            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=44100,
+            language_code="en-US",
+            audio_channel_count=2,
+            enable_automatic_punctuation=True,
+        )
+
+        operation = self.speech_client.long_running_recognize(config=config, audio=audio)
+        response = operation.result()
+
+        complete_transcription = ""
+        
+        for result in response.results:
+            # Concatenate all transcriptions from the audio file
+            complete_transcription += result.alternatives[0].transcript + " "
+        
+        return gcs_uri.split('/')[-1], complete_transcription.strip()
     
-    for result in response.results:
-        for word_info in result.alternatives[0].words:
-            word = word_info.word
-            start_time = word_info.start_time.total_seconds()
-            if hasattr(word_info, "speaker_tag"):
-                speaker_tag = word_info.speaker_tag
-            else:
-                speaker_tag = None
-            transcription_data.append({
-                "start_time": start_time,
-                "word": word,
-                "speaker_tag": speaker_tag
-            })
-            complete_transcription += " " + word
+    def process_single_file(self, file_name):
+        """Processes a single audio file for transcription."""
+        if not check_if_transcribed(file_name):
+            gcs_uri = f"gs://{self.bucket_name}/{file_name}"
+            file_name, complete_transcription = self.transcribe_audio(gcs_uri)
+            insert_transcription(file_name, complete_transcription)
+            print(f"Inserted transcription for {file_name} into the database.")
+        else:
+            print(f"Transcription for {file_name} already exists, skipping...")
 
-    transcription_data.append({"complete": complete_transcription.strip()})
-    
-    # Extracting file name from the URI
-    file_name = gcs_uri.split("/")[-1]
-    json_filename = os.path.splitext(file_name)[0] + ".json"
-    json_file_path = os.path.join(json_output_directory, json_filename)
-    with open(json_file_path, 'w', encoding='utf-8') as json_file:
-        json.dump(transcription_data, json_file, ensure_ascii=False, indent=4)
+    def process_bucket(self):
+        """Processes all audio files in the bucket for transcription using multiple threads."""
+        futures = []
+        with ThreadPoolExecutor() as executor:
+            for blob in self.storage_client.list_blobs(self.bucket_name):
+                if blob.name.endswith(".wav"):
+                    futures.append(executor.submit(self.process_single_file, blob.name))
+        for future in as_completed(futures):
+            try:
+                future.result()  # This will re-raise any exception raised during execution
+            except Exception as e:
+                print(f"An error occurred: {e}")
 
-def process_bucket():
-    """ checks if the file exists before running the transcription """
-    for blob in storage_client.list_blobs(bucket_name):
-        if blob.name.endswith(".wav"):
-            json_filename = os.path.splitext(blob.name)[0] + ".json"
-            json_file_path = os.path.join(json_output_directory, json_filename)
-            
-            if not os.path.exists(json_file_path):
-                gcs_uri = f"gs://{bucket_name}/{blob.name}"
-                transcription_data = transcribe_audio(gcs_uri)
-                # further processing of transcription_data if needed
-                print(f"Processed and saved transcription for {blob.name}")
-            else:
-                print(f"Transcription for {blob.name} already exists, skipping...")
-
-
-def process_single_file(file_name):
-    gcs_uri = f"gs://{bucket_name}/{file_name}"
-    transcription_data = transcribe_audio(gcs_uri)
-    # save or further process transcription_data
-
-# You can call process_bucket() for batch processing or process_single_file(file_name) for individual files.
 
 # Main execution
 if __name__ == "__main__":
-   # upload_files()
-    process_bucket() 
+    transcription_processor = AudioTranscription()
+    # transcription_processor.upload_files(LOCAL_DIRECTORY)
+    transcription_processor.process_bucket()
+    # Optionally call transcription_processor.process_single_file("specific_file.wav") 
